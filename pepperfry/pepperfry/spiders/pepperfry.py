3ec2c2f68918ea158c43f6f38441b8efa8b7b68b
Shashank Aggarwal <shashankaggarwal13@gmail.com>
2020-07-14 15:31:53 +0530

    Scraping pepperfry challenge complete

--- pepperfry/pepperfry/spiders/pepperfry.py
+++ pepperfry/pepperfry/spiders/pepperfry.py
@@ -1,4 +1,5 @@
 import scrapy
+import json
 import os
 import requests
 
@@ -43,8 +44,32 @@ class pepperfrySpider(scrapy.Spider):
                 yield resp
 
     def parse_response(self,response):
+        
+#         from scrapy.shell import inspect_response
+#         inspect_response(response, self)
+
+        
         image_urls = response.xpath('//li[@class="vipImage__thumb-each noClickSlide"]/a/@data-img').extract()
         item_title = response.xpath('//div/div/div/h1/text()').extract()[0]
+        item_price =  response.xpath('//div/div/div/span[@class="v-offer-price-amt  pf-medium-bold-text"]/text()').extract()[0]
+        item_savings = response.xpath('//div/div/div/span[@class="v-price-save-ttl-amt v-price-col-right total_saving"]/text()').extract()[0].split(' ')[1]
+        item_description = response.xpath('//div[@itemprop="description"]/p/text()').extract()
+        item_description = '\n'.join(item_description)
+        
+        
+        item_detail_keys = response.xpath('//div/span[@class = "v-prod-comp-dtls-listitem-label"]/text()').extract()
+        
+        item_detail_values = response.xpath('//div/span[@class = "v-prod-comp-dtls-listitem-value pf-text-grey"]/text()').extract()
+        
+        idetails = {}
+        a = len(item_detail_keys)
+        b = len(item_detail_values)
+        
+        for i in range(a):
+            item_detail_keys[i] = item_detail_keys[i][:-1]
+        
+        for i in range(min(a,b)):
+            idetails[item_detail_keys[i]] = item_detail_values[i] 
         
         CATEGORY_NAME = response.meta['dir_name']
         ITEM_DIR_URL = os.path.join(self.BASE_DIR,os.path.join(CATEGORY_NAME,item_title))
@@ -52,6 +77,17 @@ class pepperfrySpider(scrapy.Spider):
         if not os.path.exists(ITEM_DIR_URL):
             os.makedirs(ITEM_DIR_URL)
             
+        d = {
+            'Item Title':item_title,
+            'description':item_description,
+            'Item price':'Rs. '+ item_price,
+            'Savings':'Rs. '+ item_savings,
+            'Details':idetails
+        }
+            
+        with open(os.path.join(ITEM_DIR_URL,'metadata.txt'),'w') as f:
+            json.dump(d,f)
+        
         for idx,image in enumerate(image_urls):
             if idx<5:
                 with open(os.path.join(ITEM_DIR_URL,'img-{}.jpg'.format(idx)),'wb') as f:
